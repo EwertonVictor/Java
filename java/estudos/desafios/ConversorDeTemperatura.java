dc22075cd2c764bf2377147f7c99272592100f5a
Ewerton Victor <93969276+EwertonVictor@users.noreply.github.com>
2024-05-28 16:51:44 -0300

    Temperatura
    utiliza como bases o grau celsius e converte para fahrenheit

--- java/estudos/desafios/ConversorDeTemperatura.java
+++ java/estudos/desafios/ConversorDeTemperatura.java
@@ -2,6 +2,9 @@ package estudos.desafios;
 
 public class ConversorDeTemperatura {
     public static void main (String[] args){
-        
+    int celsius = 29;
+
+    double fahrenheit = (celsius * 1.8) + 32;
+    System.out.print("Temperatura: " + fahrenheit + " ºF");
     }
 }
